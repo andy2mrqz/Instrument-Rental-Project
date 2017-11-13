000ac9a217c5e02faa0d1e2808efdd639bc113fe
Andrew Marquez <andy2mgcc@gmail.com>
2017-11-13 14:21:46 -0700

    Initialized new local repository (currently up to BlowOut CP3)

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public string Index()
+        {
+            return "Please call Support at <span style='text-decoration: underline'><b>801-555-1212</b></span>. Thank you!";
+        }
+
+        public string Email(string name, string email)
+        {
+            return "Thank you " + name + ". We will send an email to " + email;
+        }
+    }
+}
---
+++ BlowOut/Controllers/RentalsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlowOut.Controllers
+{
+    public class RentalsController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Instrument(string instName, string edition)
+        {
+            ViewBag.InstrumentName = instName;
+            ViewBag.ImageSource = instName + ".jpg";
+            ViewBag.Edition = edition;
+
+            ViewBag.ImagePrices = new Dictionary<string, double[]>()
+            {
+                { "trumpet", new double[] {55, 25 } },
+                { "trombone", new double[] {60, 35 } },
+                { "tuba", new double[] {70, 50 } },
+                { "flute", new double[] {40, 25 } },
+                { "clarinet", new double[] {35, 27 } },
+                { "saxophone", new double[] {42, 30 } }
+            };
+            return View();
+        }
+    }
+}
