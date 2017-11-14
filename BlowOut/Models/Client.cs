222e3c9f6492d7058dce5832a96ec880fe2cd5f0
Andrew Marquez <andy2mgcc@gmail.com>
2017-11-13 20:41:46 -0700

    Added models for Client and Instrument with connection string to the database

---
+++ BlowOut/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlowOut.Models
+{
+    [Table("Client")]
+    public class Client
+    {
+        [Key]
+        public int clientID { get; set; }
+
+        public String firstname { get; set; }
+        public String lastname { get; set; }
+        public String address { get; set; }
+        public String city { get; set; }
+        public String state { get; set; }
+        public String zip { get; set; }
+        public String email { get; set; }
+        public String phone { get; set; }
+    }
+}
