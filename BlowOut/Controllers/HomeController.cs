1828bbec39ae05e607b583b20befdcd8e59e3e58
Andrew Marquez <andy2mgcc@gmail.com>
2017-11-14 00:52:19 -0700

    Updated CP4

--- BlowOut/Controllers/HomeController.cs
+++ BlowOut/Controllers/HomeController.cs
@@ -15,13 +15,12 @@ namespace BlowOut.Controllers
 
         public ActionResult About()
         {
-            
+
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
--- BlowOut/Controllers/RentalsController.cs
+++ BlowOut/Controllers/RentalsController.cs
@@ -3,33 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BlowOut.DAL;
+using BlowOut.Models;
+using System.Data.Entity;
+using System.Net;
 
 namespace BlowOut.Controllers
 {
+
     public class RentalsController : Controller
     {
-        
+        private BLOWOUTContext db = new BLOWOUTContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Instruments.ToList());
         }
 
-        public ActionResult Instrument(string instName, string edition)
+        public ActionResult Instrument(int instrumentID)
         {
-            ViewBag.InstrumentName = instName;
-            ViewBag.ImageSource = instName + ".jpg";
-            ViewBag.Edition = edition;
+            //Create an instrument object from the id
+            Instrument instrument = db.Instruments.Find(instrumentID);
 
-            ViewBag.ImagePrices = new Dictionary<string, double[]>()
-            {
-                { "trumpet", new double[] {55, 25 } },
-                { "trombone", new double[] {60, 35 } },
-                { "tuba", new double[] {70, 50 } },
-                { "flute", new double[] {40, 25 } },
-                { "clarinet", new double[] {35, 27 } },
-                { "saxophone", new double[] {42, 30 } }
-            };
+            return View(instrument);
+        }
+
+        public string Checkout(int instrumentID)
+        {
+            return "You are currently checking out with item: " + instrumentID;
+        }
+
+        public ActionResult Summary(int instrumentID)
+        {
             return View();
         }
     }
 }
+
+/*
+//Find the corresponding new and used IDs/prices so the customer can switch
+//Get the instrument list
+var instList = db.Instruments.ToList();
+
+//Find the new instrument information
+var newquery = from inst in instList
+               where inst.instDescription.Equals(instrument.instDescription)
+               && inst.type.Equals("new")
+               select inst;
+            //Pass the id and price to the viewbag
+            foreach (var inst in newquery)
+            {
+                ViewBag.newID = inst.instrumentID;
+                ViewBag.newPrice = inst.price;
+            }
+
+            //Find the used instrument information
+            var usedquery = from inst in instList
+                            where inst.instDescription.Equals(instrument.instDescription)
+                            && inst.type.Equals("used")
+                            select inst;
+            //Pass the id and price to the viewbag
+            foreach (var inst in usedquery)
+            {
+                ViewBag.usedID = inst.instrumentID;
+                ViewBag.usedPrice = inst.price;
+            }
+*/
